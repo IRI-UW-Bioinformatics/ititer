0645a4696e828b760390908f76cbb3c4fba80e54
davipatti <david.pattinson@wisc.edu>
2021-11-24 14:53:07 -0500

    Added sphinx_copybutton extension.

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = "0.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_copybutton"]
 autodoc_typehints = "description"
 autodoc_member_order = "bysource"
 
