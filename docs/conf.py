5cd2ac818afe5bf9ae3dd25163fd3b052ba7220e
David Pattinson <davipatti@protonmail.com>
2022-09-08 16:34:27 -0400

    Bumping version in docs/conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, David Pattinson"
 author = "David Pattinson"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
